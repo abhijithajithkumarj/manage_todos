30363a4aca241e21a95591ef58ae62f56d609aa5
ABHIJITH <aabhivishnu@gmail.com>
2024-10-28 11:27:02 +0530

    first commit

---
+++ src/main/java/manage/todos/manage_todos/ManageTodosApplication.java
@@ -0,0 +1,13 @@
+package manage.todos.manage_todos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManageTodosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManageTodosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/manage/todos/manage_todos/ManageTodosApplicationTests.java
@@ -0,0 +1,13 @@
+package manage.todos.manage_todos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManageTodosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
